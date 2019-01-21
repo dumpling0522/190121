32d009b9fa9c50f322c03baa4881e84b231205aa
水餃 <dumpling@shuijiaode-macbook-pro.local>
2019-01-21 15:28:06 +0800

    Initial Commit

---
+++ 190121Tests/_90121Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _90121Tests.swift
+//  190121Tests
+//
+//  Created by 水餃 on 2019/1/21.
+//  Copyright © 2019年 水餃. All rights reserved.
+//
+
+import XCTest
+@testable import _90121
+
+class _90121Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
